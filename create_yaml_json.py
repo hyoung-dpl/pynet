4ee37249777dabe9d0805e35d39480e8f4ea7f35
Heath Young <hyoung@denverlibrary.org>
2016-01-22 13:42:11 -0800

    Adding the finised exercise for writing out files

--- create_yaml_json.py
+++ create_yaml_json.py
@@ -8,6 +8,9 @@ import json
 
 def main():
 
+  yaml_file = 'yaml_write.yml'
+  json_file = 'json_write.json'
+
   yaml_json_dictionary = {
     'code_version':  '11.5',
     'vlan':          '404',
@@ -20,10 +23,10 @@ def main():
     'Closet 55'
   ]
 
-  with open(yaml_file, "w") as out
-    out.write(yaml.dump(yaml_json_list))
+  with open(yaml_file, "w") as out:
+    out.write(yaml.dump(yaml_json_list, default_flow_style=False))
 
-  with open(json_file, "w") as out
+  with open(json_file, "w") as out:
     json.dump(yaml_json_list, out)
 
 if __name__ == "__main__":
